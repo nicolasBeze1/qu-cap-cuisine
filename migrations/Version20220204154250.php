58b3a179b963312eb3009311c25c7ea71642896b
nicolas beze <nicolasbeze@MacBook-Pro-de-nicolas.local>
2022-02-04 16:43:38 +0100

    init bdd

--- migrations/Version20220204154250.php
+++ migrations/Version20220204154250.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220204134429 extends AbstractMigration
+final class Version20220204154250 extends AbstractMigration
 {
     public function getDescription(): string
     {
